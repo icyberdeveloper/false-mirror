08bcb8c8ee0d79d91a2a9c3c83da0ee1217fe1f9
cyber <neondelph@gmail.com>
2024-05-20 21:28:56 +0300

    update requirements

--- domain/series.py
+++ domain/series.py
@@ -1,11 +1,13 @@
 class Series:
+    external_id: str
     torrent_url: str
     name: str
     download_dir: str
     release_year: int
     season: str
 
-    def __init__(self, torrent_url: str, download_dir: str, name: str, release_year: int, season: str):
+    def __init__(self, external_id: str, torrent_url: str, download_dir: str, name: str, release_year: int, season: str):
+        self.external_id = external_id
         self.torrent_url = torrent_url
         self.name = name
         self.download_dir = download_dir
